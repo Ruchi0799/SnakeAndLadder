e8b4865b4ea6e9c4e0af453a67163788fa21ac6d
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-09 17:53:30 +0530

    Completed Creating Roll die

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,5 +2,7 @@ public class SnakeAndLadder {
     public static final int START_POSITION = 0;
     public static void main(String[] args) {
         String Player1="Ruchi";
+        double DieNumber = Math.floor(Math.random() * 10) % 6;
+        System.out.println(DieNumber);
     }
 }
