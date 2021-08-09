a07fc073f34b89b74460843d6b85e826e142aae0
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-09 18:56:48 +0530

    Completed Checking fir the player won

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -5,29 +5,42 @@ public class SnakeAndLadder {
     public static final int SNAKE = 2;
 
     public static void main(String[] args) {
-        double position=0;
-        String Player1="Ruchi";
+        double position = 0;
+        String Player1 = "Ruchi";
         double DieNumber = Math.floor(Math.random() * 10) % 6;
-        System.out.println("The die number is" + DieNumber);
-        double Action = Math.floor(Math.random() * 10) % 3;
-        System.out.println("The Action is" + Action);
+        //System.out.println("The die number is" + DieNumber);
+        //double currentposition = 0;
+        while (position < 100) {
+            double Action = Math.floor(Math.random() * 10) % 3;
 
-        switch ((int)Action)
-        {
-            case 0:
-                position=position;
-                break;
+            switch ((int) Action) {
+                case 0:
+                    position = position;
+                    System.out.println(position);
+                    break;
 
-            case 1:
-                position=position+DieNumber;
-                break;
+                case 1:
+                    position = position + DieNumber;
+                    System.out.println(position);
+                    break;
 
-            case 2:
-                position=position-DieNumber;
-                break;
+                case 2:
+                    if(position>DieNumber)
+                    {
+                        position = position - DieNumber;
+                    }
+                    else
+                    {
+                        position=0;
+                    }
+                    System.out.println(position);
+                    break;
 
 
-        }
-        System.out.println("The current position" + position);
+            }
+
+            }
+
+        System.out.println("You Won!");
     }
 }
