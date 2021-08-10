4275bb915ed9a735d1f7274eca1913abf5a120ea
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-10 15:02:54 +0530

    Made for 2 players

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -5,51 +5,94 @@ public class SnakeAndLadder {
     public static final int SNAKE = 2;
 
     public static void main(String[] args) {
-        double position = 0;
+        double position1 = 0;
+        double position2=0;
         String Player1 = "Ruchi";
         int count=0;
+        int chance=0;
 
         //System.out.println("The die number is" + DieNumber);
         //double currentposition = 0;
-        while (position < 100) {
+        while (position1 < 100 && position2<100) {
             double Action = Math.floor(Math.random() * 10) % 3;
             double DieNumber = Math.floor(Math.random() * 10) % 6;
             count++;
+            chance++;
             switch ((int) Action) {
                 case 0:
-                    position = position;
-                    //System.out.println(position);
+                    if (chance%2==0)
+                    {
+                        position1 = position1;
+                        System.out.println("PLAYER1:"+position1);
+                    }
+                    else if(chance%2==1)
+                    {
+                        position2=position2;
+                        System.out.println("PLAYER2:"+position2);
+                    }
+
                     break;
 
                 case 1:
-                    if(position+DieNumber<=100) {
-                        position = position + DieNumber;
-                        System.out.println(position);
-                    }
-                    else
+                    if(chance%2==0)
                     {
-                        position=position;
+                        if (position1 + DieNumber <= 100)
+                        {
+                            position1 = position1 + DieNumber;
+                            System.out.println("PLAYER1:"+position1);
+                        }
+                        else
+                        {
+                            position1 = position1;
+                        }
+                    }
+                    else if(chance%2==1)
+                    {
+                        if (position2 + DieNumber <= 100)
+                        {
+                            position2 = position2 + DieNumber;
+                            System.out.println("PLAYER2:"+position2);
+                        }
+                        else
+                        {
+                            position2 = position2;
+                        }
                     }
 
                     break;
 
                 case 2:
-                    if(position>DieNumber)
-                    {
-                        position = position - DieNumber;
+                    if (chance%2==0) {
+                        if (position1 > DieNumber) {
+                            position1 = position1 - DieNumber;
+                        } else {
+                            position1 = 0;
+                        }
+                        System.out.println("PLAYER1:"+position1);
                     }
-                    else
+                    else if(chance%2==1)
                     {
-                        position=0;
+                        if (position2 > DieNumber) {
+                            position2 = position2 - DieNumber;
+                        } else {
+                            position2 = 0;
+                        }
+                        System.out.println("PLAYER2:"+position2);
                     }
-                    //System.out.println(position);
                     break;
 
 
             }
 
         }
-
-        System.out.println("You Won after rolling die for " + count + " Times");
+        System.out.println("End Position of player 1:" +position1);
+        System.out.println("End Position of player 2:" +position2);
+        if (position1==100) {
+            System.out.println(" PLAYER1 Won after rolling die for " + count + " Times");
+        }
+        else if(position2==100)
+        {
+            System.out.println(" PLAYER2 Won after rolling die for " + count + " Times");
+        }
     }
 }
